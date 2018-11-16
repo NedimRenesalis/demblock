292b33f23ccc543a1f6bc9565a165ff7ec5eb7c6
Kerim.Alihodza <kerim.alihodza@isatis.com>
2018-11-16 15:17:11 +0100

    Reset password : 1 commit

--- frontend/views/site/zaboravljena-lozinka/BA-zaboravljena-lozinka.php
+++ frontend/views/site/zaboravljena-lozinka/BA-zaboravljena-lozinka.php
@@ -7,37 +7,32 @@ use yii\helpers\Html;
 $this->title = 'Zapošljavanje';
 ?>
 
-<div class="section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <img src="<?= Url::to('@web/css/images/password-forgotten.jpg'); ?>" class="img-responsive">
-            </div>
-            <div class="col-md-4">
+<div class="sign-container">
+<div class="login" style="
+    margin: nuset;
+    display: flex;
+    align-items: center;
+    margin: 50px auto;"
+>
+    <div class="sign-header">
+        <h2>RESET PASSWORD</h2>
+    </div>
+    <div style="width: 340px; margin: auto;" class="reset-password">
+        <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form', 'fieldConfig' => ['template' => '{label}{input}']]); ?>
 
-                <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form', 'fieldConfig' => ['template' => '{label}{input}']]); ?>
+        <?= $form->field($model, 'email')->textInput(['autofocus' => true])->label("Za promjenu lozinke - unesite Vaš e-mail") ?>
 
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true])->label("Za promjenu lozinke - unesite Vaš e-mail") ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Pošalji zahtijev', ['class' => 'active btn btn-block btn-primary']) ?>
-                </div>
-
-                <?php ActiveForm::end(); ?>
-
-            </div>
-            <div class="col-md-4">
-                <img src="<?= Url::to('@web/css/images/password-forgotten.jpg'); ?>" class="img-responsive">
-            </div>
+        <div class="form-group">
+            <?= Html::submitButton('Pošalji zahtijev', ['class' => 'active btn btn-block btn-primary']) ?>
         </div>
+ 
+            <div style="">
+                <p class="text-center">U slučaju eventualnih problema – kontaktirajte nas na</p>
+                <a>oglasi@zaposljavanje.ba</a>
+            </div>
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
-<div class="section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <p class="text-center">U slučaju eventualnih problema – kontaktirajte nas na oglasi@zaposljavanje.ba</p>
-            </div>
-        </div>
-    </div>
+    
+
 </div>
