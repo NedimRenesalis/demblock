61a000a671d8b3af3a271e4da2695110056b1c0d
Kerim <kerimalihodza22@gmail.com>
2018-11-19 23:04:39 +0100

    Cjenovnik

--- frontend/views/site/cjenovnik-usluge/BA-cjenovnik-usluge.php
+++ frontend/views/site/cjenovnik-usluge/BA-cjenovnik-usluge.php
@@ -10,95 +10,143 @@ $this->title = 'Zapošljavanje';
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <h3 class="text-center"><b>Cijenovnik oglasa i opis usluga</b></h3>
-                <br>
-                <br><b>Ovaj cijenovnik vrijedi samo za pravna lica koja su fiskalni rezident u BiH.</b>
-<br>
+                <h3 class="text-center sign-header" style="color: white;"><b>Cijenovnik oglasa i opis usluga</b></h3>
+                
+                <br> <strong>Ovaj cijenovnik vrijedi samo za pravna lica koja su fiskalni rezident u BiH.</strong></br  >
 
-                <br>Za pravna lica koja nisu fiskalni rezident u BiH vrijedi cijenovnik na engleskom jeziku kao i maska za objavu oglasa na engleskom jeziku.</u></a>
+
+                Za pravna lica koja nisu fiskalni rezident u BiH vrijedi cijenovnik na engleskom jeziku kao i maska za objavu oglasa na engleskom jeziku.</u></a>
                 </div>
         </div>
     </div>
 </div>
-<div class="section">
+<div class="services" style="
+    margin-top: 20px;">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 text-center">
-                <img src="<?= Url::to('@web/css/images/price.png') ?>" class="img-responsive">
-                <h3 class="text-center">
+            <div class="text-center service-card" style="margin: 5px; border-right: 1px solid white;">
+                <h3 class="service-header">
                     <strong>CLASSIC Oglas</strong>
                 </h3>
-                <p class="text-center">Oglas unutar kategorije po poziciji - 30 dana - 35KM (+ PDV 17% = 40,95KM)</p>
-<br>Ova vrsta oglasa je rezultat pretrage u tražilici.
 
-                <br>* * * *
-                <br>
+                <img src="<?= Url::to('@web/css/images/price.png') ?>" class="img-responsive">
+
+                <div class="service-content">
+                    <div class="describe">
+                        Ova vrsta oglasa je rezultat pretrage u tražilici.
+                    </div>
+                    <div class="prices">
+                        <div>
+                            Oglas unutar kategorije po poziciji - 30 dana - 35KM (+ PDV 17% = 40,95KM)
+                        </div>
+                        * * * *
+
+                    </div>
+
+                    
+                </div>
+                
             </div>
-            <div class="col-md-4 text-center">
-                <img src="<?= Url::to('@web/css/images/price2.png') ?>" class="img-responsive">
-                <h3 class="text-center">
+            <div class="text-center service-card">
+                <h3 class="service-header" style="background: #0077B5;">
                     <strong>MIDI Oglas</strong>
                 </h3>
-                <p class="text-justify">Ovaj tip oglasa je idealan za poslodavce koji istovremeno oglašavaju više
-                    od jednog oglasa i kojima je bitno osigurati vidljivost svih oglasa na
-                    naslovnoj strani odmah ispod Platinum oglasa.</p>
-                <br>- 1 pozcija 30 dana: 70KM
-                <br>(+ PDV 17% = 81,9KM)
-                <br>
-                <br>- 2 do 5 pozicija 30 dana: 120KM
-                <br>(+ PDV 17% = 140,4KM)
-                <br>
-                <br>- 6 do 10 pozicija 30 dana: 170KM
-                <br> (+ PDV 17% = 198,9KM)
-                <br>
-                <br>- preko 11 pozicija 30 dana: 220KM
-                <br>(+ PDV 17% = 257,4KM)
-                <br>
-                <br>* * * *</div>
-            <div class="col-md-4 text-center">
-                <img src="<?= Url::to('@web/css/images/price3.png')?>" class="img-responsive">
-                <h3 class="text-center">
+
+                <img src="<?= Url::to('@web/css/images/price2.png') ?>" class="img-responsive">
+                
+                <div class="service-content">
+                    <div class="describe">
+                        Ovaj tip oglasa je idealan za poslodavce koji istovremeno oglašavaju više
+                        od jednog oglasa i kojima je bitno osigurati vidljivost svih oglasa na
+                        naslovnoj strani odmah ispod Platinum oglasa
+                    </div>
+                    <div class="prices">
+                        <div>
+                            - 1 pozcija 30 dana: 70KM
+                            (+ PDV 17% = 81,9KM)
+                        </div>
+                        <div>
+                            - 2 do 5 pozicija 30 dana: 120KM
+                            (+ PDV 17% = 140,4KM)
+                        </div>
+                        
+                        <div>
+                            - 6 do 10 pozicija 30 dana: 170KM
+                             (+ PDV 17% = 198,9KM)
+                        </div>
+                        <div>
+                            - preko 11 pozicija 30 dana: 220KM
+                            (+ PDV 17% = 257,4KM)
+                        </div>
+                        
+                        
+                        * * * *
+                    </div>
+                    
+                </div>
+            </div>
+                
+            <div class="text-center service-card" style="margin: 5px; border-left: 1px solid white;">
+                <h3 class="text-center service-header" style="background: #127a15;">
                     <strong>PLATINUM Oglas</strong>
                 </h3>
-                <p class="text-center">Platinum oglasi predstavljaju prvoklasni način oglašavanja uz veoma konkurentne
-                    cijene na najvidljivijem mjestu portala zaposljavanje.ba , na naslovnoj
-                    strani odmah ispod maske za pretraživanje.
-                    <br>
-                    <br>
 
+                <img src="<?= Url::to('@web/css/images/price3.png')?>" class="img-responsive">
+                
+                <div class="service-content">
+                    <div class="describe">
+                        Platinum oglasi predstavljaju prvoklasni način oglašavanja uz veoma konkurentne
+                        cijene na najvidljivijem mjestu portala zaposljavanje.ba , na naslovnoj
+                        strani odmah ispod maske za pretraživanje.
+                    </div>
 
+                    <div class="prices">
+                        <div>
+                            Platinum Oglas 15 dana: 100KM (+ PDV 17% = 117KM)
+                            
+                        </div>
+                        <div>
+                            Platinum Oglas 30 dana: 150KM (+ PDV 17% = 175,5KM)
+                            
+                        </div>
+                        <div>
+                            Platinum Oglas 40 dana: 200KM (+ PDV 17% = 234KM)
+                            
+                        </div>
+                    </div>
 
-                    <br>
-                    <br>Platinum Oglas 15 dana: 100KM (+ PDV 17% = 117KM)
-                    <br>
-                    <br>Platinum Oglas 30 dana: 150KM (+ PDV 17% = 175,5KM)
-                    <br>
-                    <br>Platinum Oglas 40 dana: 200KM (+ PDV 17% = 234KM)
-                    <br>
-                    <br>
-                    <strong>Zašto odabrati zaposljavanje.ba?</strong>
-                    <br>
-                    <br>
-                    <strong>Kod nas prvoklasno oglašavanje za radno mjesto neće koštati kao jedna
-                        prosječna neto plata.</strong>
-                    <br>
-                    <br>Sa nama efikasno pronađite idealne kandidate - a novac umjesto u skupe
-                    oglase - investirajte u razvoj ljudskih resursa.
-                    <br>
-                    <br>* * * *</p>
-                <br>
+                    
+                        <div class="others" style="border-bottom: 1px solid gray;">
+                        <strong>Zašto odabrati zaposljavanje.ba?</strong>
+                        
+                        
+                        <strong>Kod nas prvoklasno oglašavanje za radno mjesto neće koštati kao jedna
+                            prosječna neto plata.</strong>
+                        
+                        <div>Sa nama efikasno pronađite idealne kandidate - a novac umjesto u skupe
+                        oglase - investirajte u razvoj ljudskih resursa.</div>
+                        
+
+                        
+                        </div>
+                    <div>
+                    * * * *
+
+                    </div>
+                        
+                </div>
             </div>
         </div>
     </div>
-</div>
-<div class="section">
+
+    <div class="section">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <p class="text-center"><b>Prije objave oglasa molimo da se registrujete kao poslodavac.</b>
-                  <br>
+                  
 
-                  <br><b>Plaćanje oglasa</b>
+                <br><b>Plaćanje oglasa</b>
                   <br>Oglasi se mogu platiti odmah putem kreditne kartice online ili putem uplatnice na osnovu predračuna.
                   <br>
                   <br>Ako odaberete uplatu putem  uplatnice kontaktirate nas emailom na <a href="mailto:#">oglasi@zaposljavanje.ba</a> ili putem telefona na 062-332-325 za izdavanje predračuna. U email-u navedite ime Vaše firme i vrstu oglasa koji želite objaviti.
@@ -132,3 +180,5 @@ $this->title = 'Zapošljavanje';
         </div>
     </div>
 </div>
+</div>
+
