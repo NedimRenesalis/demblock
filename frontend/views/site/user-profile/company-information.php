4782baf2558c0f042422622b3a1e80f71e443284
Kerim <kerimalihodza22@gmail.com>
2018-11-24 16:27:18 +0100

    Company-information

--- frontend/views/site/user-profile/company-information.php
+++ frontend/views/site/user-profile/company-information.php
@@ -10,22 +10,23 @@ UploadAsset::register($this);
 /* @var $model app\models\CompanyInformation */
 /* @var $form ActiveForm */
 ?>
-<div class="site-user-profile-company-information">
+<div class="info-edit">
+    <h3>EDIT COMPANY INFORMATIONS</h3>
 
     <?php $form = ActiveForm::begin(); ?>
-    <?php $date = DateTime::createFromFormat("Y-m-d", $model->Year);
-    $model->Year =  $date->format("Y"); ?>
-        <?= $form->field($model, 'Year')->textInput(['type' => 'number', 'min' => 1800]) ?>
-        <?= $form->field($model, 'NumberOfEmployees')->textInput(['type' => 'number', 'min' => 0]) ?>
-        <?= $form->field($model, 'AboutUs') ?>
-        <?= $form->field($model, 'CompanyName') ?>
-        <?= $form->field($model, 'Website') ?>
-        <?= $form->field($model, 'RegisteredAddress') ?>
-        <?= $form->field($model, 'OperationalAddress') ?>
-    
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
-        </div>
+        <?php// $date = DateTime::createFromFormat("Y-m-d", $model->Year);
+        // $model->Year =  $date->format("Y"); ?>
+            <?= $form->field($model, 'Year')->textInput(['type' => 'number', 'min' => 1800]) ?>
+            <?= $form->field($model, 'NumberOfEmployees')->textInput(['type' => 'number', 'min' => 0]) ?>
+            <?= $form->field($model, 'AboutUs') ?>
+            <?= $form->field($model, 'CompanyName') ?>
+            <?= $form->field($model, 'Website') ?>
+            <?= $form->field($model, 'RegisteredAddress') ?>
+            <?= $form->field($model, 'OperationalAddress') ?>
+        
+            <div class="form-group">
+                <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
+            </div>
     <?php ActiveForm::end(); ?>
 
-</div><!-- site-user-profile-company-information -->
+</div>
