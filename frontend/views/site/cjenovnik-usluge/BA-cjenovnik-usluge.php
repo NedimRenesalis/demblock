1d0062d939563fecd350a4ccf2e7ae635a6b67c1
Programiranje <nedim@renesalis-packaging.com>
2019-01-08 12:08:04 +0100

    n
    Signed-off-by: Programiranje <nedim@renesalis-packaging.com>

--- frontend/views/site/cjenovnik-usluge/BA-cjenovnik-usluge.php
+++ frontend/views/site/cjenovnik-usluge/BA-cjenovnik-usluge.php
@@ -10,12 +10,18 @@ $this->title = 'Zapošljavanje';
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <h3 class="text-center sign-header" style="color: white;"><b>Cijenovnik oglasa i opis usluga</b></h3>
+                <h3 class="text-center sign-header" style="color: white;"><b>LISTING FEES</b></h3>
 
-                <br> <strong>Ovaj cijenovnik vrijedi samo za pravna lica koja su fiskalni rezident u BiH.</strong></br  >
+                <br> <strong>These listing fees only include product listing services on our marketplace for a selected time period without corporate and sourcing verification.</strong>
+<br>
+				<br>Product listings expire and get delisted after the selected time period expires - and can be relisted for the below mentioned fees again -  whereas corporate and sourcing verification is paid only once to get notarized as decentralized information on the blockchain immutably for an infinite period of time. Both services - the product listing as well as the corporate and sourcing verification only can be paid with our tokens.
 
-
-                Za pravna lica koja nisu fiskalni rezident u BiH vrijedi cijenovnik na engleskom jeziku kao i maska za objavu oglasa na engleskom jeziku.</u></a>
+                <br>More information about corporate and sourcing verification can be found in the "Corporate verification" section.
+               
+			   <br><br>Our product listing packages are: Prime, Selected and Classic listings. They all include the same extent of product listing features, but are very different considering their visibility on our marketplace. Prime listings are always visible directly without search on the landing page on the top. After them the Selected listings are listed on the landing page. The Classic listings are only visible as results to direct searches in our product search engine.
+			   
+			   <br><br>Nevertheless, prior listing your product we recommend to verify your corporate details and sourcing information on the blockchain, which can - with our tokens - seamlessly be done directly on our marketplace. Thus you as a seller will increase trust and transparency towards your customers.  
+			   </u></a>
                 </div>
         </div>
     </div>
@@ -28,24 +34,24 @@ $this->title = 'Zapošljavanje';
 						<div class="g-table5 gprice-single">
 							<!-- Table Head -->
 							<div class="gprice-head">
-								<h2 class="g-title">Basic</h2>
+								<h2 class="g-title">Prime</h2>
 								<!-- Table Price -->
 								<div class="g-amount">
-									<p class="amount">$49</p>
+									<p class="amount">20 </p>
 								</div>
 							</div>
 							<!-- Table List -->
 							<ul class="gprice-list">
-								<li>5 GB Ram</li>
-								<li>40GB SSD Cloud Storage</li>
-								<li>Month Subscription</li>
-								<li>Responsive Framework</li>
-								<li class="cross">Monthly Billing Software</li>
-								<li class="cross">1 Free Website</li>
+							<li>LISTING FEES</li>
+								<li>20 tokens for 15 days</li>
+								<li>30 tokens for 30 days</li>
+								<li>40 tokens for 40 days</li>
+								<li>Most visible listing</li>
+							
 							</ul>
 							<!-- Table Bottom -->
 							<div class="gprice-bottom">
-								<a class="g-button bordered radius" href="#">Signup Now</a>
+								<a class="g-button bordered radius" href="#">Good deal</a>
 							</div>
 						</div>
 					</div>
@@ -55,24 +61,23 @@ $this->title = 'Zapošljavanje';
 						<div class="g-table5 gprice-single">
 							<!-- Table Head -->
 							<div class="gprice-head">
-								<h2 class="g-title">Standard</h2>
+								<h2 class="g-title">Selected</h2>
 								<!-- Table Price -->
 								<div class="g-amount">
-									<p class="amount">$79</p>
+									<p class="amount">25</p>
 								</div>
 							</div>
 							<!-- Table List -->
 							<ul class="gprice-list">
-								<li>5 GB Ram</li>
-								<li>40GB SSD Cloud Storage</li>
-								<li>Month Subscription</li>
-								<li>Responsive Framework</li>
-								<li class="cross">Monthly Billing Software</li>
-								<li class="cross">1 Free Website</li>
+								<li>LISTING FEES</li>
+								<li>25 tokens for 30 days</li>
+								<li>Listed below Prime listings</li>
+								<li>Suitable for companies with multiple products</li>
+								<li>Listing duration 30 days</li>
 							</ul>
 							<!-- Table Bottom -->
 							<div class="gprice-bottom">
-								<a class="g-button bordered radius" href="#">Signup Now</a>
+								<a class="g-button bordered radius" href="#">Optimal listing fees</a>
 							</div>
 						</div>
 					</div>
@@ -82,24 +87,25 @@ $this->title = 'Zapošljavanje';
 						<div class="g-table5 gprice-single">
 							<!-- Table Head -->
 							<div class="gprice-head">
-								<h2 class="g-title">Premium</h2>
+								<h2 class="g-title">Classic</h2>
 								<!-- Table Price -->
 								<div class="g-amount">
-									<p class="amount">$99</p>
+									<p class="amount">10</p>
 								</div>
 							</div>
 							<!-- Table List -->
 							<ul class="gprice-list">
-								<li>5 GB Ram</li>
-								<li>40GB SSD Cloud Storage</li>
-								<li>Month Subscription</li>
-								<li>Responsive Framework</li>
-								<li class="cross">Monthly Billing Software</li>
-								<li class="cross">1 Free Website</li>
+							<li>LISTING FEES</li>
+							<li>10 tokens for 30 days</li>
+								<li>Listing only visible in search result list</li>
+								<li>Not listed on landing page</li>
+								<li>Very competitive listing price</li>
+								
+								
 							</ul>
 							<!-- Table Bottom -->
 							<div class="gprice-bottom">
-								<a class="g-button bordered radius" href="#">Signup Now</a>
+								<a class="g-button bordered radius" href="#">Most Affordable Listing</a>
 							</div>
 						</div>
 					</div>
@@ -111,8 +117,8 @@ $this->title = 'Zapošljavanje';
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <a class="active btn btn-block btn-primary" style="height: 37px;margin-bottom: 24px;" href="<?= Url::to('objava-oglasa'); ?>">
-                    Nastavite dalje sa unosom detalja za oglas
+                
+				
                 </a>
             </div>
         </div>
