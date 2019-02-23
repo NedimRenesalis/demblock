b1fdeb65ce526a8ad3e4dcfe73328e30aa8fc0ad
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2019-02-23 16:33:41 +0100

    updading production

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export MYSQL_DATABASE="zaposljavanje"
+export MYSQL_USER="zaposljavanje"
+
 # Build and apply migrations
 docker-compose up --build -d
 docker exec web_demblock_machinepicker composer install --no-interaction
