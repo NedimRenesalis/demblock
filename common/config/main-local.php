5374c498d626b195183fe4fa2e03facb24f22e43
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2019-02-23 17:57:59 +0100

    Revert "update prod"
    This reverts commit d417c3a6fd4bf13a6ef44232d58a13b98b6b5f58.

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=mysql;dbname='.getenv('MYSQL_DATABASE').'',
-            'username' => ''.getenv('MYSQL_USER').'',
-            'password' => ''.getenv('MYSQL_PASSWORD').'',
+            'dsn' => 'mysql:host=mysql;dbname='.getenv("MYSQL_DATABASE").'',
+            'username' => ''.getenv("MYSQL_USER").'',
+            'password' => ''.getenv("MYSQL_PASSWORD").'',
             'charset' => 'utf8',
         ],
         'mailer' => [
