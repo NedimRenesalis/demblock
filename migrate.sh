7fcd0d8cebcfe328aec5c260c1df41d4eb7601bc
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2019-02-23 19:13:24 +0100

    Delete migrate.sh

--- migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export MYSQL_DATABASE=zaposljavanje
-export MYSQL_USER=zaposljavanje
-
-# Build and apply migrations
-docker-compose up --build -d
-docker exec web_demblock_machinepicker /bin/bash -c 'php yii migrate --interactive=0'
