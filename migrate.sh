410404c73724c09f325ec1348de1268ce09b860c
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2019-02-23 12:15:02 +0100

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -2,4 +2,4 @@
 
 # Build and apply migrations
 docker-compose up -d --build
-docker exec web_demblock_login php yii migrate --interactive=0
+docker exec web_demblock_machinepicker php yii migrate --interactive=0
