77347cf2d3a93a9fee43b427feb418a3e8e5f423
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2019-02-27 21:09:06 +0100

    updating mailer

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -9,16 +9,23 @@ return [
             'charset' => 'utf8',
         ],
         'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com',
-                'username' => 'support@demblock.com',
-                'password' => '123Lolakola!!',
-                'port' => '587',
-                'encryption' => 'tls',
-            ],
+          'class' => 'yii\swiftmailer\Mailer',
+          'viewPath' => '@common/mail',
+          'transport' => [
+              'class' => 'Swift_SmtpTransport',
+              'host' => 'smtp.gmail.com',
+              'username' => 'support@demblock.com',
+              'password' => '123Lolakola!!',
+              'port' => '587',
+              'encryption' => 'tls',
+              'streamOptions' => [
+                  'ssl' => [
+                      'allow_self_signed' => true,
+                      'verify_peer' => false,
+                      'verify_peer_name' => false,
+                  ],
+              ],
+          ],
         ],
     ],
 ];
