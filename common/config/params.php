97cc9094be80b4e43c5da540e4a2f98011e4a0b8
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2019-03-01 11:01:31 +0100

    updating machinepicker + demblock

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,17 @@ return [
     'adminEmail' => 'support@demblock.com',
     'supportEmail' =>  'support@demblock.com',
     'user.passwordResetTokenExpire' => 3600,
+    
+    // Machinepicker + Demblock sale config
+    'offChainServer' => "https://token.demblock.com",
+    'postItemDapp' => "https://token.demblock.com/list-item.html",
+    'itemInfoDapp' => "https://token.demblock.com/user-items.html",
+    'allowedDomains'  => [
+        'http://token.demblock.com'
+    ],
+
+    // Static
+    'userQuery' => "user",
+    'userIdQuery' => "userId",
+    'userExistsQuery' => "modelExists",
 ];
--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,13 +1,4 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'offChainServer' => "http://localhost:3000",
-    'postItemDapp' => "http://localhost:8080/list-item.html",
-    'itemInfoDapp' => "http://localhost:8080/user-items.html",
-    'userQuery' => "user",
-    'userIdQuery' => "userId",
-    'userExistsQuery' => "modelExists",
-    'allowedDomains'  => [
-        'http://localhost:8080'
-    ]
+    'adminEmail' => 'admin@example.com'
 ];
